92eec35ab226f64352ca9806480ebc3e54cec971
João  Vitor <134068768+joaovitorlopes18@users.noreply.github.com>
2023-10-26 11:49:19 -0300

    Add files via upload
    feito por João Vitor  e Cibele

---
+++ informa____es_pessoais (3).sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 26-Out-2023 às 16:42
+-- Versão do servidor: 10.4.28-MariaDB
+-- versão do PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `harry potter`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `informações pessoais`
+--
+
+CREATE TABLE `informações pessoais` (
+  `Bruxo` int(100) NOT NULL,
+  `Coruja eletronica` int(100) NOT NULL,
+  `Senha` varchar(100) NOT NULL,
+  `DataNasc` varchar(31) NOT NULL,
+  `HoraNasc` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
